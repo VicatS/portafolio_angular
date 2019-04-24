c29cdd59a71a284ec790b9e0aaf02ac32adbcf72
Victor <victorcs2704@gmail.com>
2019-04-24 12:12:09 -0400

    Routing en proceso

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const APP_ROUTES: Routes = [
+    { path: '', component: PortafolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'item', component: ItemComponent },
+    { path: '**', pathMatch: 'full', redirectTo: '' }
+];
+@NgModule({
+    imports: [
+        RouterModule.forRoot(APP_ROUTES)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule { }
